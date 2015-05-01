c4850fd674f306ac6bffc0cd9b0190e11e34df3f
Kamran Shahzad <bleak.unseen@gmail.com>
2015-05-01 14:43:47 +0500

    initial files added

---
+++ DependencyInjection/KamranThemeExtension.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Kamran\ThemeBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+
+class KamranThemeExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $this->processConfiguration($configuration, $configs);
+
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+
+        $container->setParameter('twig.form.resources', array_merge(
+            $container->getParameter('twig.form.resources'),
+            array('KamranThemeBundle:Form:overwrite_form_themes.html.twig')
+        ));
+    }
+}
